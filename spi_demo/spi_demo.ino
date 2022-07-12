65b67b7123ba8843838dc0163d3fdb9bc5812627
BOGO <42817357+CamelBOGO@users.noreply.github.com>
2022-07-12 22:51:28 +0800

    Moved Files

--- spi_demo/spi_demo.ino
+++ spi_demo/spi_demo.ino
@@ -1,11 +1,55 @@
-#include <LedControl.h>
 #define SPI_bitRead(value, bit) (((value) >> (bit)) & 0x01)
 
-int SPI_Pin_MOSI = 11;
-int SPI_Pin_CS = 10;
-int SPI_Pin_CLK = 13;
+// Global Variables
+int SPI_Pin_MOSI = 11;	//MOSI
+int SPI_Pin_CS = 10;	//CS
+int SPI_Pin_CLK = 13;	//clk
 
-LedControl lc = LedControl(SPI_Pin_MOSI, SPI_Pin_CLK, SPI_Pin_CS, 0);
+// Function Prototypes
+void spiInt(int SPI_Pin_MOSI, int SPI_Pin_CS, int SPI_Pin_CLK);
+void spiWrite(byte opcode, byte data);
+void clearDisplay();
+void test01();
+/*------------------------------Main---------------------------------*/
+
+
+void setup() {
+	spiInt();
+
+	//   lc.shutdown(0, false);
+	spiWrite(0x0c, 1);
+
+	//   lc.setIntensity(0, 5);     // Adjust the brightness maximum is 15
+	spiWrite(0x0A, 5);
+
+	//   lc.clearDisplay(0);
+	clearDisplay();
+	test01();
+	while (1);
+}
+
+void loop() {
+}
+
+/*------------------------------Defined Function---------------------------------*/
+
+//initialize the SPI protocol
+void spiInt(int SPI_Pin_MOSI, int SPI_Pin_CS, int SPI_Pin_CLK){
+	pinMode(SPI_Pin_MOSI, OUTPUT);
+	pinMode(SPI_Pin_CLK, OUTPUT);
+	pinMode(SPI_Pin_CS, OUTPUT);
+	digitalWrite(SPI_Pin_CS, HIGH);
+
+	spiWrite(0X0F, 0);       //Display test
+
+	spiWrite(0x0B, 0x07);        //scan limit
+
+	spiWrite(0x09, 0);       //Decode mode
+
+	clearDisplay();
+
+	spiWrite(0x0C, 0);       //Shutdown true
+}
 
 void test01() {
 	//Create an array with the data to shift out
@@ -16,13 +60,31 @@ void test01() {
 	//enable the line
 	digitalWrite(SPI_Pin_CS, LOW);
 
-	for (int i = 7; i >= 0; i--) {// 8 bits in a byte
-
+	for (int i = 7; i >= 0; i--) { // 8 bits in a byte
 		digitalWrite(SPI_Pin_MOSI, SPI_bitRead(opcode, i));   // Set MOSI
 		digitalWrite(SPI_Pin_CLK, HIGH);                  // SCK high
 		digitalWrite(SPI_Pin_CLK, LOW);                   // SCK low
 	}
 
+	for (int i = 7; i >= 0; i--) { // 8 bits in a byte
+		digitalWrite(SPI_Pin_MOSI, SPI_bitRead(data, i));   // Set MOSI
+		digitalWrite(SPI_Pin_CLK, HIGH);                  // SCK high
+		digitalWrite(SPI_Pin_CLK, LOW);                   // SCK low
+	}
+
+	digitalWrite(SPI_Pin_CS, HIGH);
+}
+
+void spiWrite(byte opcode, byte data) {
+	//enable the line
+	digitalWrite(SPI_Pin_CS, LOW);
+
+	for (int i = 7; i >= 0; i--) { // 8 bits in a byte
+
+		digitalWrite(SPI_Pin_MOSI, SPI_bitRead(opcode, i));   // Set MOSI
+		digitalWrite(SPI_Pin_CLK, HIGH);                  // SCK high
+		digitalWrite(SPI_Pin_CLK, LOW);                   // SCK low
+	}
 	for (int i = 7; i >= 0; i--) { // 8 bits in a byte
 
 		digitalWrite(SPI_Pin_MOSI, SPI_bitRead(data, i));   // Set MOSI
@@ -33,15 +95,8 @@ void test01() {
 	digitalWrite(SPI_Pin_CS, HIGH);
 }
 
-void setup() {
-  
-
-  lc.shutdown(0, false);
-  lc.setIntensity(0, 5);     //Adjust the brightness maximum is 15
-  lc.clearDisplay(0);
-  test01();
-  while (1);
-}
-
-void loop() {
+void clearDisplay() {
+	for (int i = 0; i < 8; i++) {
+		spiWrite(i + 1, 0);
+	}
 }
