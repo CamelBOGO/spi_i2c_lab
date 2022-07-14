9fa5042969e71e9c0a681b8f0e0da9a8b18896f7
MichaelShek <100515575+MichaelShek@users.noreply.github.com>
2022-07-14 12:27:49 +0800

    spi

--- spi_demo/spi_demo.ino
+++ spi_demo/spi_demo.ino
@@ -5,6 +5,7 @@ uint8_t spiMosiPin = 11;  // MOSI
 uint8_t spiCsPin = 10;    // CS
 uint8_t spiClkPin = 13;   // clk
 uint8_t spiMisoPin = 9;	  // MISO
+ int numOfDevices = 2;	  // Number of the max7219-matrix Dot devices
 
 // Function Prototypes
 void spiInit(uint8_t spiMosiPin, uint8_t spiCsPin, uint8_t spiClkPin);
@@ -29,7 +30,7 @@ void loop() {}
 
  // Initialize the SPI protocol.
 void spiInit(uint8_t spiMosiPin, uint8_t spiCsPin, uint8_t spiClkPin) {
-	// Set MOSI, CLK and CS pin to output mode.
+	// Set MOSI, CLK, MISO and CS pin to output mode.
 	pinMode(spiMosiPin, OUTPUT);
 	pinMode(spiClkPin, OUTPUT);
 	pinMode(spiCsPin, OUTPUT);
@@ -41,14 +42,15 @@ void spiInit(uint8_t spiMosiPin, uint8_t spiCsPin, uint8_t spiClkPin) {
 }
 
 void MAX7219Init(){
-	// Some display initializations.
-	spiWrite(0x09, 0);		// Set Decode-mode register to no decode for digit 0-7
-	spiWrite(0x0A, 5);		// Set Intensity register to adjust the brightness, maximum is 15.	
-	spiWrite(0x0B, 0x07);	// Set scan-limit register to determine how many digits(0-7) are displayed
-	spiWrite(0x0C, 1);  	// Set Shutdown register to normal operation
-	spiWrite(0X0F, 0);		// Set Display-test register to normal operation
-
-	clearDisplay();
+	// Some MAX7219 initializations.
+	for(int i=0;i<numOfDevices;i++){
+	spiWrite(i,0x09, 0);		// Set Decode-mode register to no decode for digit 0-7
+	spiWrite(i,0x0A, 5);		// Set Intensity register to adjust the brightness, maximum is 15.	
+	spiWrite(i,0x0B, 0x07);	// Set scan-limit register to determine how many digits(0-7) are displayed
+	spiWrite(i,0x0C, 1);  	// Set Shutdown register to normal operation
+	spiWrite(i,0X0F, 0);		// Set Display-test register to normal operation
+	}
+	clearDisplay();			// Initialize display
 
 }
 
@@ -58,7 +60,7 @@ void test01() {
 	byte opcode = 0x02;
 	byte data = 0b11100011;
 
-	spiWrite(opcode, data);
+	spiWrite(0,opcode, data);
 }
 
 // SPI write a byte without enable/disable.
@@ -74,7 +76,7 @@ void spiWriteByte(byte data) {
 }
 
 // SPI write 2 bytes with enable/disable.
-void spiWrite(byte opcode, byte data) {
+void spiWrite(int addr,byte opcode, byte data) {
 	// Enable the line.
 	digitalWrite(spiCsPin, LOW);
 
@@ -82,13 +84,21 @@ void spiWrite(byte opcode, byte data) {
 	spiWriteByte(opcode);
 	spiWriteByte(data);
 
+	while(addr){
+		spiWriteByte(0);
+		spiWriteByte(0);
+		addr--;
+	}
+
 	// Disable the line.
 	digitalWrite(spiCsPin, HIGH);
 }
 
 void clearDisplay() {
 	// Use spiWrite instead?
-	for (int i = 0; i < 8; i++) {
-		spiWrite(i + 1, 0);
+	for(int j=0;j<numOfDevices;j++){
+		for (int i = 0; i < 8; i++) {
+			spiWrite(j,i + 1, 0);
+		}
 	}
 }
